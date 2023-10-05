154690b9bac1f6963c2cf306cbb56f9eacb0ea9e
Sahit <sahit.berisha11@gmail.com>
2023-10-05 10:40:40 +0200

    added github workflow

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@sahitberisha11',
   },
+  pathPrefix: '/portfolio',
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
